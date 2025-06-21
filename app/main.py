684369b88386106c534a6e33406da63e445048b1
Filipe Almeida <filipesilva504@gmail.com>
2025-06-21 19:31:26 -0400

    retoandno prefixo

--- app/main.py
+++ app/main.py
@@ -20,10 +20,10 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.include_router(auth.router)
-app.include_router(users.router)
-app.include_router(courses.router)
+app.include_router(auth.router, prefix="/v1")
+app.include_router(users.router, prefix="/v1")
+app.include_router(courses.router, prefix="/v1")
 
-@app.get("/")
+@app.get("/v1/")
 def root():
     return {"message": "Hello World"}
